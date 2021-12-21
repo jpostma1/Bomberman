21e35b4323786658cad73a0b968939e65abc460d
Jelly <jedydemon@gmail.com>
2021-12-21 23:41:52 +0100

    Added some types in KeyboardInput

--- src/Input/KeyboardInput.ts
+++ src/Input/KeyboardInput.ts
@@ -1,6 +1,5 @@
 let keyMap:any = {};
 let keyTime:any = {};
-let shortcutControl = "inactive"
 
 
 export let keyCodes:any = {enter:13, shift:16, ctrl:17, alt:18, caps:20, esc:27, a:65, b:66, c:67, d:68, e:69, f:70, g:71, h:72, i:73, j:74, k:75, l:76, m:77, n:78, o:79, p:80, q:81, r:82, s:83, t:84, u:85, v:86, w:87, x:88, y:89, z:90, space:32, left:37, up:38, right:39, down:40,
@@ -10,13 +9,11 @@ export let keyCodes:any = {enter:13, shift:16, ctrl:17, alt:18, caps:20, esc:27,
 for(let key in keyCodes)
     keyTime[keyCodes[key]] = -1
 
-console.log("keyTime", keyTime)
-
 class KeyJustPressedListener {
     customKeyJustPressedFunctions:any[] = []
 
     // NOTE: add type of function with para1:KeyboardEvent, para2:string
-    addKeyJustPressedFunction(func:any) {
+    addKeyJustPressedFunction(func:(e:KeyboardEvent) => void) {
         this.customKeyJustPressedFunctions.push(func)
     }
 }
@@ -54,19 +51,19 @@ export function clearKeyMap() {
         keyMap[key] = false;
 }
 
-export function keyJustPressed(keyEvent:KeyboardEvent, keyNum:any) {
+export function keyJustPressed(keyEvent:KeyboardEvent, keyNum:string | number) {
     return keyEvent.keyCode == keyCodes[keyNum] && keyTime[keyCodes[keyNum]] == -1 && keyEvent.type == "keydown"
 }
 
-export function keyJustReleased(keyEvent:KeyboardEvent, keyNum:any) {
+export function keyJustReleased(keyEvent:KeyboardEvent, keyNum:string | number) {
     return keyEvent.keyCode == keyCodes[keyNum] && keyEvent.type == "keyup"
 }
 
-export function keyPressed(key:any) {
+export function keyPressed(key:string | number) {
     return keyMap[keyCodes[key]];
 }
 
-export function keyHolded(key:any, msConsideredHolding = 200) {
+export function keyHolded(key:string | number, msConsideredHolding = 200) {
     let keycode = keyCodes[key];
 
     if(msConsideredHolding <= 0)
