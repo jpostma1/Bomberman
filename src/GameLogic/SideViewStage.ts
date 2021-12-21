eb806f99fcbf9d3081eb1577d08081e03dff6234
Jelly <jedydemon@gmail.com>
2021-12-21 09:49:03 +0100

    Moved SideViewStage to its own file

---
+++ src/GameLogic/SideViewStage.ts
@@ -0,0 +1,56 @@
+import { getTileHeight, getTileWidth } from "../Rendering/DrawFunctions";
+import { Sprite, Container } from "pixi.js";
+
+
+
+export class SideViewStage {
+
+    tileWidth: number;
+    tileHeight: number;
+
+    zIndexRowOffset: number = 10;
+    playerZOffset: number = 3;
+    bombZOffset: number = 2;
+    itemZOffset: number = 1;
+
+    container: Container;
+    constructor(tileColumns: number, tileRows: number, tileWidth: number, tileHeight: number) {
+        this.container = new Container();
+        this.container.width = tileColumns * getTileWidth();
+        this.container.height = tileRows * getTileHeight();
+
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+    }
+
+    toScreenCoordX(x: number) {
+        return x * this.tileWidth;
+    }
+
+    toScreenCoordY(y: number) {
+        return y * this.tileHeight;
+    }
+    getTileZIndexFromY(y: number): number {
+        return Math.floor(y) * this.zIndexRowOffset;
+    }
+
+    getPlayerZIndexFromY(y: number): number {
+        return Math.floor(y) * this.zIndexRowOffset + this.playerZOffset;
+    }
+
+    getBombZIndexFromY(y: number): number {
+        return Math.floor(y) * this.zIndexRowOffset + this.bombZOffset;
+    }
+
+    getItemZIndexFromY(y: number): number {
+        return Math.floor(y) * this.zIndexRowOffset + this.itemZOffset;
+    }
+
+    addChild(child: Sprite) {
+        this.container.addChild(child);
+    }
+
+    removeChild(child: Sprite) {
+        this.container.removeChild(child);
+    }
+}
